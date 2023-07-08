c46bac23b43ccbe46efa42352c52fab870ba8850
leandro.jesus0004@gmail.com <Mega123z>
2023-07-08 14:58:31 -0300

    .NET 7

---
+++ ProEventos/ProEventos.Models/Database/Evento.cs
@@ -0,0 +1,13 @@
+﻿namespace ProEventos.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public String Local { get; set; } = string.Empty;
+        public String DataEvento { get; set; } = string.Empty;
+        public string Tema { get; set; } = string.Empty;
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; } = string.Empty;
+        public string ImagemURL { get; set; } = string.Empty;
+    }
+}
---
+++ ProEventos/ProEventos/Controllers/EventoController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.Models;
+
+namespace ProEventos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]
+        {
+            new Evento()
+            {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "1 Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+            },
+            new Evento()
+            {
+                EventoId = 2,
+                Tema = "Angular 11 e .NET 7",
+                Local = "Belo Hor33333333izonte",
+                Lote = "1222 Lote",
+                QtdPessoas = 2501,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.333png"
+            }
+        };
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id) ;
+        }
+
+        [HttpPost]
+        public int Post()
+        {
+            return 123;
+        }
+
+        [HttpDelete]
+        public int Deletes()
+        {
+            return 1;
+        }
+    }
+
+
+}
